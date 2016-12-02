dddaff291cda9fd1b5dba052dc099c3640a23857
Gustav Collin Rasmussen <gcr@novataris.com>
2016-12-02 10:47:03 +0100

    Added basic login form

--- WebApplication1/Controllers/AuthController.cs
+++ WebApplication1/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +12,37 @@ namespace WebApplication1.Controllers
         public ActionResult Login()
         {
             //return Content("Login!");
-            return View();
+            return View(new AuthLogin
+                {
+                    //Test = "This is my test value set in my controller!"
+                });
         }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            //return Content("Hey there, " + form.Username);
+            //form.Test = "This is my test value set in my post action!";
+            //return View(form);
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            if (form.Username != "rainbow dash")
+            {
+                ModelState.AddModelError("Username", "Username or password isn't 20% cooler.");
+                return View(form);
+            }
+
+            return Content("The form is valid!");
+
+
+
+        }
+
+
+
+
+
     }
 }
---
+++ WebApplication1/ViewModels/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.ViewModels
+{
+    public class AuthLogin
+    {
+        //public string Test { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
