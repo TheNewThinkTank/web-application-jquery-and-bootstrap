8c5f3d51f8fd3727131522582c14895b9884aca7
Gustav Collin Rasmussen <gcr@novataris.com>
2017-01-04 09:05:07 +0100

    Database created with FluentMigrator

---
+++ WebApplication1/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using FluentMigrator;
+using FluentMigrator.Builders;
+
+namespace WebApplication1.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
+                .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+
+        }
+        public override void Down()
+        {
+            Delete.Table("role_users");
+            Delete.Table("roles");
+            Delete.Table("users");
+
+
+
+        }
+
+
+    }
+}
---
+++ WebApplication1/Tools/DeployDb-Dev.bat
@@ -0,0 +1,1 @@
+..\..\packages\FluentMigrator.1.1.1.0\tools\Migrate.exe --db=mysql --target=..\bin\WebApplication1.dll --ConfigPath=..\Web.config -c=MainDb
